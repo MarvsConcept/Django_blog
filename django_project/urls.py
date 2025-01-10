817595f2fb980f45570c6297800c9936e1bf1e16
MarvsConcept <marvade8@gmail.com>
2025-01-10 23:54:51 +0100

    Added Profile Image

--- django_project/urls.py
+++ django_project/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 #Auth
 from django.contrib.auth import views as auth_views
+
 
 
 
@@ -31,3 +34,6 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
