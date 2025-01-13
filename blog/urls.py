59b80f2b39154d8a2a80faf0def5bad94d7a4914
MarvsConcept <marvade8@gmail.com>
2025-01-13 18:52:14 +0100

    Added Ordering and Detail view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
-
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<pk>', views.PostDetailView.as_view(), name='blog-home'),
     path('about/', views.about, name = 'blog-about')
 ]
